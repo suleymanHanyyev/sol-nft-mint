b2e15ebe75d36adba85d2f89c4d48b0dc1a1ca69
Suleyman Hanyyev <suleyman.hanyyev@ug.bilkent.edu.tr>
2022-05-30 19:54:52 +0200

    Initial commit, phantom wallet connected

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -7,6 +7,31 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  const checkIfWalletIsConnected = async() => {
+    try {
+      const {solana} = window;
+      if ( solana && solana.isPhantom){
+        const response = await solana.connect({ onlyIfTrusted: true });
+        console.log('Connected with Public Key:',response.publicKey.toString());
+        console.log("Phantom Wallet Found!");
+      } else {
+        alert("No phantom wallet detected");
+      }
+    } catch(error) {
+      console.log("Error while connecting phantom", error);
+    }
+  };
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+  
   return (
     <div className="App">
       <div className="container">
