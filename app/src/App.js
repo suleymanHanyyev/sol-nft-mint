b89dfa7ebd896fb56e2980e3e7fa73dc19d4ddbf
Suleyman Hanyyev <suleyman.hanyyev@ug.bilkent.edu.tr>
2022-06-01 16:04:06 +0200

    It works, need an rpc but lazy

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-
+import CandyMachine from './CandyMachine';
 // Constants
 const TWITTER_HANDLE = 'Solomon_nsi';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
@@ -73,7 +73,7 @@ const App = () => {
           <p className="sub-text">NFT drop machine with fair mint</p>
           {!walletAddress && renderNotConnectedContainer()}
         </div>
-
+          { walletAddress && <CandyMachine walletAddress={window.solana} />}
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
